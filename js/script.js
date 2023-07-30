fcdbf955567dcfb6e2ae934475f1338774fdcf5f
LucasBeccaria <lucasjulianbeccaria17@gmail.com>
2023-07-30 17:44:45 -0300

    (guardado sass)

--- js/script.js
+++ js/script.js
@@ -31,7 +31,7 @@ document.querySelector('#menu-btn').onclick = () =>{
     navbar.classList.toggle('active');
     searchForm.classList.remove('active');
     cart.classList.remove('active');
-    loginForm.classList.remove('active');
+    loginForm.classList.remove('active'); 
 }
 
 window.onscroll = () =>{
@@ -39,4 +39,19 @@ window.onscroll = () =>{
     cart.classList.remove('active');
     loginForm.classList.remove('active');
     navbar.classList.remove('active');
+}
+
+let slides = document.querySelectorAll('.home .slides-container .slide');
+let index = 0;
+
+function next(){
+    slides[index].classList.remove('active');
+    index = (index + 1) % slides.length
+    slides[index].classList.add('active');
+}
+
+function prev(){
+    slides[index].classList.remove('active');
+    index = (index - 1 + slides.length) % slides.length
+    slides[index].classList.add('active');
 }
